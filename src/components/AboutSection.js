7870ea9e3538d67e680a25d8a4d4e383489a10e5
Faraji <farajisparks@gmail.com>
2023-06-23 13:07:51 -0500

    About animations

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,12 +1,11 @@
 import React from "react";
 
+import Wave from "./Wave";
 //Styles
 //import styled from 'styled-components';
 import {About, Description, Image, Hide} from '../styles';
-
-
 import {motion} from 'framer-motion';
-
+import {titleAnim, fade, photoAnim} from '../animation'
 const AboutSection = () =>{
 
 
@@ -15,20 +14,20 @@ const AboutSection = () =>{
             <Description>
                 <motion.div className="title"> 
                     <Hide>
-                        <motion.h2> I work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}> I work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >your <span>moments</span></motion.h2>
+                        <motion.h2 variants={titleAnim}>your <span>moments</span></motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>come to life</motion.h2>
+                        <motion.h2 variants={titleAnim}>come to life</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Some extra details you'd like to say about yourself
-                </p>
-                <button>Contact me</button>
+                <motion.p variants={fade}>Some extra details you'd like to say about yourself
+                </motion.p>
+                <motion.button variants={fade}>Contact me</motion.button>
                 <Image>
-                    <img src="https://i.ibb.co/2d9MN7P/DSC-0339.jpg" alt="brandon center"/>
+                    <motion.img variants={photoAnim} src="https://i.ibb.co/2d9MN7P/DSC-0339.jpg" alt="brandon center"/>
                 </Image>
             </Description>
         </About>
